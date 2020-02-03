2916fa01fc01858580439b8103353346deeb3095
rikutoman <r.k.t.1019@icloud.com>
2020-02-03 22:09:14 +0900

    config/route.rb-change

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
+  resources :microposts
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'users#index'
+
+  #ユーザー一覧を表示
+  get '/users', to: 'users#index'
+  #新規投稿（登録画面）に遷移
+  get '/users/new', to: 'users#new'
+  #投稿されたデータを受け取る
+  post '/users', to: 'users#create'
+
+  #投稿されてデータを編集する画面に行く
+  get '/users/:id/edit', to: 'users#edit'
+
+  #編集完了画面に遷移
+  patch 'users/:id', to: 'users#update'
+
+  # 投稿の削除を実施する
+  delete '/users/:id', to:'users#destroy'
 end
